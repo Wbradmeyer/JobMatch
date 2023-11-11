ecc376d6855e32721179b4f90ec5a74486c65fde
Brad Meyer <wbradmeyer@gmail.com>
2023-11-11 11:05:25 -0500

    created company reg page

---
+++ client/src/components/CompanyRegister.jsx
@@ -0,0 +1,79 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const CompanyRegister = () => {
+  const navigate = useNavigate();
+  const [companyUser, setCompanyUser] = useState({
+    name: "",
+    location: "",
+    aboutUs: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleRegisterChange = (e) => {
+    setCompanyUser({
+      ...companyUser,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleRegisterSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("", companyUser, { withCredentials: true })
+      .then((res) => {
+        console.log(res.data);
+        localStorage.setItem("currentUser", JSON.stringify(res.data));
+        navigate("/company/dashboard");
+      })
+      .catch((err) => {
+        console.log(err);
+        // Set errors for Validations here
+      });
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <div>
+        <form onSubmit={handleRegisterSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              onChange={handleRegisterChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="location">Location:</label>
+            <input
+              type="text"
+              name="location"
+              id="location"
+              onChange={handleRegisterChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="aboutUs">About Us:</label>
+            <textarea
+              name="aboutUs"
+              id="aboutUs"
+              cols="30"
+              rows="10"
+              onChange={handleRegisterChange}
+            ></textarea>
+          </div>
+          <button>Register</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CompanyRegister;
