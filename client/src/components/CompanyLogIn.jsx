1161a9a15b38f8805c03e83b769fde0dc0366c03
Matthew Reep <reepmatt20@gmail.com>
2023-11-15 14:21:09 -0500

    Company Dashboard done with link styling changes

--- client/src/components/CompanyLogIn.jsx
+++ client/src/components/CompanyLogIn.jsx
@@ -54,7 +54,7 @@ const CompanyLogIn = () => {
               <input type="password" id='password' name='password' onChange={handleCompanyChange} className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'/>
             </div>
             <div>
-            <Link to={'/seekerRegister'} className='text-sm font-medium text-primary-600 hover:underline dark:text-primary-500'>Company Register</Link>
+            <Link to={'/seekerRegister'} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Company Register</Link>
             </div>
             <button className='w-full bg-sky-300 hover:bg-sky-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>Log-In</button>
           </form>
