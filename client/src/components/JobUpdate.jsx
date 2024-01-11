ce09c235f630a6df6b9ba9116d8c49e0368f3882
Brad Meyer <wbradmeyer@gmail.com>
2024-01-11 10:19:36 -0500

    edit page back button

--- client/src/components/JobUpdate.jsx
+++ client/src/components/JobUpdate.jsx
@@ -94,18 +94,20 @@ const JobUpdate = () => {
 
   return (
     <div className="ml-10">
-      <h1 className="mb-2 mt-0 text-5xl font-medium leading-tight">
-        Edit This Job
-      </h1>
-      <div className="flex justify-end w-3/4 mx-auto">
-        <p className="text-sm font-medium  text-gray-900">
-          <Link
-            to={"/company/dashboard"}
-            className="inline-block rounded-full border-2 border-neutral-800 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-800 transition duration-150 ease-in-out hover:border-neutral-800 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-800 focus:border-neutral-800 focus:text-neutral-800 focus:outline-none focus:ring-0 active:border-neutral-900 active:text-neutral-900 dark:border-neutral-900 dark:text-neutral-900 dark:hover:border-neutral-900 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10 dark:hover:text-neutral-900 dark:focus:border-neutral-900 dark:focus:text-neutral-900 dark:active:border-neutral-900 dark:active:text-neutral-900"
-          >
-            back to dashboard
-          </Link>
-        </p>
+      <div className="flex items-center my-4">
+        <h1 className="mb-2 mt-0 text-5xl font-medium leading-tight">
+          Edit This Job
+        </h1>
+        <div className="flex justify-end w-1/2 mx-auto">
+          <p className="text-sm font-medium text-gray-900">
+            <Link
+              to={"/company/dashboard"}
+              className="my-5 font-medium text-xl text-blue-600 dark:text-blue-500 hover:underline py-2 px-2 bg-slate-400 rounded-lg"
+            >
+              Back to Dashboard
+            </Link>
+          </p>
+        </div>
       </div>
       <div>
         <form onSubmit={onSubmitHandler}>
